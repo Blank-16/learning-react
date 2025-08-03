1728713791ab822efd4108787453a9b9c8a37bff
Blank-16 <ansumanpal16@gmail.com>
2025-08-03 17:16:17 +0530

    refactor: improve AuthService class structure and error handling

--- BloggingWeb/src/appwrite/auth.js
+++ BloggingWeb/src/appwrite/auth.js
@@ -6,13 +6,53 @@ export class AuthService {
     client = new Client();
     account;
 
-    constructor() 
-    {
+    constructor() {
         this.client
             .setEndpoint(config.appwriteUrl)
             .setEndpoint(config.appwriteProjectId);
         this.account = new Account(this.client)
-        
+    }
+
+    async createAccount({ email, password, name }) {
+        try {
+
+            const userAccount = await this.account.create(ID.unique(), email, password, name);
+            if (userAccount) {
+                // call another method
+                return this.login({ email, password });
+            } else {
+                return userAccount;
+            }
+        } catch (error) {
+            throw new Error("Appwrite Error in createAccount");
+
+        }
+    }
+
+    async login({ email, password }) {
+        try {
+            return await this.account.createEmailPasswordSession(email, password);
+        } catch (error) {
+            throw new Error("Appwrite Error in login");
+        }
+    }
+
+    async getCurrentUser() {
+        try {
+            return await this.account.get();
+        } catch (error) {
+            throw new Error("Appwrite Error in getUsername");
+        }
+
+        return null;
+    }
+
+    async logout() {
+        try {
+            await this.account.deleteSessions();
+        } catch (error) {
+            throw new Error("Appwrite service errro logout");
+        }
     }
 
 }
