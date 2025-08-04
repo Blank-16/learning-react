8cb46095e43217b6ad601fc90da6d08ce39c75de
Blank-16 <ansumanpal16@gmail.com>
2025-08-05 01:40:02 +0530

    feat: add Header, Footer, Container, Logo, and LogoutBtn components with navigation and layout

---
+++ BloggingWeb/src/components/Logo.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+
+function Logo({
+    width = "100px"
+}) {
+  return (
+    <div className="" width = {width}>
+        IMG
+    </div>
+  )
+}
+
+export default Logo
--- BloggingWeb/src/components/index.js
+++ BloggingWeb/src/components/index.js
@@ -1,9 +1,15 @@
 import Header from "./Header";
 import Footer from "./Footer";
+import Container from "./Container";
+import Logo from "./Logo";
+import LogoutBtn from "./LogoutBtn";
 
 
 export {
     Header,
     Footer,
+    Logo,
+    Container,
+    LogoutBtn,
 
 }
