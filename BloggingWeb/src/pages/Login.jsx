db9e30ed398e3a6f2c7c2b38710e11139e54fc35
Blank-16 <ansumanpal16@gmail.com>
2025-08-18 06:41:12 +0530

    feat: add AddPost, AllPosts, EditPost, Home, Login, Post, and Signup components for post management and user authentication

---
+++ BloggingWeb/src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Login as loginComponents} from '../components/index'
+
+
+function Login() {
+    return (
+        <div className="py-8">
+            <loginComponents />
+        </div>
+    )
+}
+
+export default Signup
